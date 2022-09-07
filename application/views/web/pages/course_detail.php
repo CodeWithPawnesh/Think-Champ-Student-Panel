ae6474eb074bccc6f809b38e84dc0f405be90662
CodeWithPawnesh <96884856+CodeWithPawnesh@users.noreply.github.com>
2022-09-07 12:44:36 +0530

    Few Changes in form

--- application/views/web/pages/course_detail.php
+++ application/views/web/pages/course_detail.php
@@ -150,7 +150,7 @@ marketer, project manager, product manager, technical recruiter, or entrepreneur
 
 <section class="course-third-section" style="background-image:url(assets/images/courses/section-2bg.jpg);">
     <div class="course-form-container">
-        <h1>Course Form</h1>
+        <h1>Enroll Now</h1>
     <form action="" class="course-form">
         <div class="course-form-left-col">
         <h4>Full Name</h4>
@@ -159,31 +159,40 @@ marketer, project manager, product manager, technical recruiter, or entrepreneur
         <input type="email"  placeholder="Enter Your E-mail Address" class="course-form-input" /><br><br>
         <h4>Phone Number</h4>
         <input type="number"  placeholder="Enter Your Phone Number" class="course-form-input" /><br><br>
-        <h4>Select Course</h4>
+        <h4>Select Your Desired Batch </h4>
+        <select name="batch-time" id="batch-time" >
+        <option value="forth-slot">Select Any One Option</option>
+        <option value="forth-slot">8:00 A.M to 10:00 A.M</option>
+        <option value="first-slot">11:00 A.M to 01:00 P.M</option>
+        <option value="second-slot">2:00 P.M to 04:00 P.M</option>
+        <option value="third-slot">5:00 P.M to 07:00 P.M</option>
+        </select><br><br>
+        </div>
+        <div class="course-form-right-col">
+        <h4>Select a Course</h4>
          <select name="course" id="course" >
+         <option value="forth-slot">Select Any One Option</option>
         <option value="php">PHP</option>
         <option value="python">PYHTON</option>
         <option value="js">JS</option>
         <option value="react-js">REACT-JS</option>
         </select><br><br>
-        </div>
-        <div class="course-form-right-col">
-        <h4>Batch Time</h4>
-        <select name="batch-time" id="batch-time" >
-        <option value="forth-slot">8:00 A.M to 10:00 A.M</option>
-        <option value="first-slot">11:00 A.M to 01:00 P.M</option>
-        <option value="second-slot">2:00 P.M to 04:00 P.M</option>
-        <option value="third-slot">5:00 P.M to 07:00 P.M</option>
-       
-        </select><br><br>
-        <h4>Select</h4>
+        <h4>Who You Are?</h4>
         <select name="student" id="student" >
+        <option value="forth-slot">Select Any One Option</option>
         <option value="forth-slot">Student</option>
         <option value="first-slot">Professional</option>
         <option value="second-slot">Others</option>
         </select><br><br>
         <input type="text"  placeholder="Enter Your College/Company Name and Designation" class="course-form-input" /><br><br>
-        </div>
+        <h4>Gender</h4>
+        <select name="student" id="student" >
+        <option value="forth-slot">Select Any One Option</option>
+        <option value="forth-slot">Male</option>
+        <option value="first-slot">Female</option>
+        <option value="second-slot">Other</option>
+        </select>
+    </div>
         <button type="submit"  value="Submit" class="course-submit-btn">Submit</button>
     </form>
     
